13eed99224fe8633a3759ca98c14bf6d525cb8d7
Ealipse <100053423+Ealipse@users.noreply.github.com>
2026-04-29 23:23:54 +0200

    update 0.0.1

--- maps.js
+++ maps.js
@@ -1,26 +1,387 @@
 const maps = [
   {
-    name: "WhiteSpire",
-    image: "images/WhiteSpire02.jpg",
-    tm: "https://trackmania.exchange/mapshow/208015",
-    yt: "https://www.youtube.com/watch?v=R7tb8_xavFI"
+    name: "Future Of The Past",
+    images: [
+      "https://trackmania.exchange/mapimage/246195/1"
+    ],
+    tm: "https://trackmania.exchange/mapshow/246195",
+    yt: "https://youtu.be/Ie19Jy-3-J4",
+    info: [
+      "Tag: RPG",
+      "Author: ProjectSynergy, Ealipse, Laku27, Nixotica",
+      "Map times: 00:52.815",
+      "Uploaded: 25 May 2025"
+    ]
   },
   {
     name: "Muspelheim",
-    image: "images/MuspelheimIngame.jpg",
+    images: [
+      "https://trackmania.exchange/mapimage/248210/1"
+    ],
     tm: "https://trackmania.exchange/mapshow/248210",
-    yt: "https://www.youtube.com/watch?v=bET68r_GRak"
-  },
-  {
-    name: "Gilneas",
-    image: "images/Gilneas.jpeg",
-    tm: "https://trackmania.exchange/mapshow/59472",
-    yt: "https://www.youtube.com/watch?v=T931m7m281E"
+    yt: "https://youtu.be/bET68r_GRak",
+    info: [
+      "Tag: Ice, Scenery",
+      "Author: Ealipse, Thounej",
+      "Map times: 00:47.925",
+      "Uploaded: 3 Jun 2025"
+    ]
   },
   {
     name: "E39",
-    image: "images/E39.jpg",
+    images: [
+      "https://trackmania.exchange/mapimage/281877/1",
+      "https://trackmania.exchange/mapimage/281877/2",
+      "https://trackmania.exchange/mapimage/281877/3",
+      "https://trackmania.exchange/mapimage/281877/4",
+      "https://trackmania.exchange/mapimage/281877/5"
+    ],
     tm: "https://trackmania.exchange/mapshow/281877",
-    yt: ""
+    yt: "https://youtu.be/yPGrnI0nvWI",
+    info: [
+      "Tag: SpeedTech, Scenery",
+      "Author: Ealipse",
+      "Map times: 00:32.340",
+      "Uploaded: 5 Dec 2025"
+    ]
+  },
+  {
+    name: "CCP#24 - Northpole",
+    images: [
+      "https://trackmania.exchange/mapimage/147513/1"
+    ],
+    tm: "https://trackmania.exchange/mapshow/147513",
+    yt: "https://youtu.be/XWIOiWUjtwA",
+    info: [
+      "Tag: Dirt, Scenery",
+      "Author: Vixxa, Ealipse",
+      "Map times: 00:51.623",
+      "Uploaded: 24 Dec 2023"
+    ]
+  },
+  {
+    name: "Jungle 1212",
+    images: [
+      "https://trackmania.exchange/mapimage/145917/"
+    ],
+    tm: "https://trackmania.exchange/mapshow/145917",
+    yt: "",
+    info: [
+      "Tag: Offroad, Scenery, Endurance",
+      "Author: Clearvision, Waypoint Mapping, entrylag, Ealipse, Whiskey.wp",
+      "Map times: 04:31.212",
+      "Uploaded: 12 Dec 2023"
+    ]
+  },
+  {
+    name: "Portside Pursuit",
+    images: [
+      "https://trackmania.exchange/mapimage/144786/"
+    ],
+    tm: "https://trackmania.exchange/mapshow/144786",
+    yt: "",
+    info: [
+      "Tag: MultiLap, Competitive, Scenery",
+      "Author: TheChraz, Ealipse",
+      "Map times: 03:31.558",
+      "Uploaded: 3 Dec 2023"
+    ]
+  },
+  {
+    name: "Symplegades",
+    images: [
+      "https://trackmania.exchange/mapimage/135629/"
+    ],
+    tm: "https://trackmania.exchange/mapshow/135629",
+    yt: "https://youtu.be/k3CKY-Tx7mg",
+    info: [
+      "Tag: Tech, Dirt, Scenery",
+      "Author: Vixxa, Ealipse, tatauna",
+      "Map times: 00:54.025",
+      "Uploaded: 16 Oct 2023"
+    ]
+  },
+  {
+    name: "Parthenon 2",
+    images: [
+      "https://trackmania.exchange/mapimage/108643/"
+    ],
+    tm: "https://trackmania.exchange/mapshow/108643",
+    yt: "",
+    info: [
+      "Tag: Dirt, Scenery",
+      "Author: ",
+      "Map times: ",
+      "Uploaded: "
+    ]
+  },
+  {
+    name: "MTC - Artemis 11",
+    images: [
+      "https://trackmania.exchange/mapimage/91428/",
+      "https://trackmania.exchange/mapimage/91428/2",
+      "https://trackmania.exchange/mapimage/91428/3",
+      "https://trackmania.exchange/mapimage/91428/4"
+    ],
+    tm: "https://trackmania.exchange/mapshow/91428",
+    yt: "https://www.youtube.com/@stammproductions9620",
+    info: [
+      "Tag: SpeedFun, Scenery, Grass",
+      "Author: Juice, deadfish, Ealipse, agrabou.wp",
+      "Map times: 00:48.500",
+      "Uploaded: 29 Jan 2023"
+    ]
+  },
+  {
+    name: "MTC - Clio 22",
+    images: [
+      "https://trackmania.exchange/mapimage/94748/"
+    ],
+    tm: "https://trackmania.exchange/mapshow/94748",
+    yt: "https://youtu.be/9VzLJy4v-RI",
+    info: [
+      "Tag: Dirt, Scenery, Transitional",
+      "Author: Osoguineapig, Ealipse",
+      "Map times: 00:45.000",
+      "Uploaded: 19 Feb 2023"
+    ]
+  },
+  {
+    name: "CCP#25 - Blafjell",
+    images: [
+      "https://trackmania.exchange/mapimage/85185/"
+    ],
+    tm: "https://trackmania.exchange/mapshow/85185",
+    yt: "https://youtu.be/-F260orfQJo",
+    info: [
+      "Tag: Ice, Scenery, Transitional",
+      "Author: Scarzor, Ealipse",
+      "Map times: 00:46.822",
+      "Uploaded: 25 Dec 2022"
+    ]
+  },
+  {
+    name: "New Formula",
+    images: [
+      "https://trackmania.exchange/mapimage/82841/"
+    ],
+    tm: "https://trackmania.exchange/mapshow/82841",
+    yt: "https://youtu.be/FJ6Ac9PF1fw",
+    info: [
+      "Tag: MultiLap, SpeedFun, Scenery",
+      "Author: Nightwolf93, TheChraz, Ealipse",
+      "Map times: 03:20.953",
+      "Uploaded: 3 Dec 2022"
+    ]
+  },
+  {
+    name: "Champions Drift",
+    images: [
+      "https://trackmania.exchange/mapimage/62184/",
+      "https://trackmania.exchange/mapimage/62184/2",
+      "https://trackmania.exchange/mapimage/62184/3",
+      "https://trackmania.exchange/mapimage/62184/4",
+      "https://trackmania.exchange/mapimage/62184/5"
+    ],
+    tm: "https://trackmania.exchange/mapshow/62184",
+    yt: "https://youtu.be/pZzpZcjp66A",
+    info: [
+      "Tag: SpeedFun, Competitive, Scenery",
+      "Author: Nightwolf93, TheChraz, Ealipse, Wirtual",
+      "Map times: 00:57.492",
+      "Uploaded: 19 May 2022"
+    ]
+  },
+  {
+    name: "Gilneas",
+    images: [
+      "https://trackmania.exchange/mapimage/59472/",
+      "https://trackmania.exchange/mapimage/59472/2",
+      "https://trackmania.exchange/mapimage/59472/3",
+      "https://trackmania.exchange/mapimage/59472/4",
+      "https://trackmania.exchange/mapimage/59472/5"
+    ],
+    tm: "https://trackmania.exchange/mapshow/59472",
+    yt: "https://www.youtube.com/watch?v=T931m7m281E",
+    info: [
+      "Tag: Scenery, Mixed, Transitional",
+      "Author: Vixxa, Ealipse",
+      "Map times: 01:04.228",
+      "Uploaded: 7 May 2022"
+    ]
+  },
+  {
+    name: "Rorvik",
+    images: [
+      "https://trackmania.exchange/mapimage/54242/",
+      "https://trackmania.exchange/mapimage/54242/2",
+      "https://trackmania.exchange/mapimage/54242/3",
+      "https://trackmania.exchange/mapimage/54242/4",
+      "https://trackmania.exchange/mapimage/54242/5"
+    ],
+    tm: "https://trackmania.exchange/mapshow/54242",
+    yt: "https://youtu.be/oC1K3zTdcAs",
+    info: [
+      "Tag: SpeedFun, Scenery, Grass",
+      "Author: deadfish, Ealipse",
+      "Map times: 00:49.797",
+      "Uploaded: 6 Mar 2022"
+    ]
+  },
+  {
+    name: "Bobtech",
+    images: [
+      "https://trackmania.exchange/mapimage/56156/",
+      "https://trackmania.exchange/mapimage/56156/2",
+      "https://trackmania.exchange/mapimage/56156/3"
+    ],
+    tm: "https://trackmania.exchange/mapshow/56156",
+    yt: "https://youtu.be/Unh1kLvB2T8",
+    info: [
+      "Tag: Tech, Educational",
+      "Author: deadfish, Osoguineapig, Ealipse",
+      "Map times: 00:50.726",
+      "Uploaded: 31 Mar 2022"
+    ]
+  },
+  {
+    "name": "FASTER Downhill",
+    "images": [
+      "https://trackmania.exchange/mapimage/222714/1"
+    ],
+    "tm": "https://trackmania.exchange/mapshow/222714",
+    "yt": "",
+    "info": [
+      "Tag: Dirt",
+      "Author: Waypoint Mapping, Ealipse",
+      "Map times: 01:00.561",
+      "Uploaded: 24 Jan 2025"
+    ]
+  },
+  {
+    "name": "FASTER Everest",
+    "images": [
+      "https://trackmania.exchange/mapimage/306662/1"
+    ],
+    "tm": "https://trackmania.exchange/mapshow/306662",
+    "yt": "",
+    "info": [
+      "Tag: Ice",
+      "Author: Waypoint Mapping, Ealipse, Popouette_TM",
+      "Map times: 01:12.000",
+      "Uploaded: 3 Apr 2026"
+    ]
+  },
+  {
+    "name": "FASTER Soapbox",
+    "images": [
+      "https://trackmania.exchange/mapimage/310319/1"
+    ],
+    "tm": "https://trackmania.exchange/mapshow/310319",
+    "yt": "",
+    "info": [
+      "Tag: Competitive, Engine Off",
+      "Author: Waypoint Mapping, Ealipse, Keissla.wp, Jaxrud",
+      "Map times: 01:10.000",
+      "Uploaded: 19 Apr 2026"
+    ]
+  },
+  {
+    "name": "Ranchpoint",
+    "images": [
+      "https://trackmania.exchange/mapimage/173768/1"
+    ],
+    "tm": "https://trackmania.exchange/mapshow/173768",
+    "yt": "https://youtu.be/WxFISiZzOaU",
+    "info": [
+      "Tag: Tech, Dirt, Scenery",
+      "Author: mime.wp, Ealipse",
+      "Map times: 00:50.864",
+      "Uploaded: 29 May 2024"
+    ]
+  },
+  {
+    "name": "Whitespire",
+    "images": [
+      "https://trackmania.exchange/mapimage/208015/1"
+    ],
+    "tm": "https://trackmania.exchange/mapshow/208015",
+    "yt": "",
+    "info": [
+      "Tag: Scenery, Transitional, Grass",
+      "Author: Ealipse, Phil TM",
+      "Map times: 00:49.212",
+      "Uploaded: 11 Nov 2024"
+    ]
+  },
+  {
+    "name": "Back'N'Forth",
+    "images": [
+      "https://trackmania.exchange/mapimage/206599/1"
+    ],
+    "tm": "https://trackmania.exchange/mapshow/206599",
+    "yt": "",
+    "info": [
+      "Tag: MultiLap, Ice, Scenery",
+      "Author: Ealipse",
+      "Map times: 00:42.799",
+      "Uploaded: 2 Nov 2024"
+    ]
+  },
+  {
+    "name": "Starforge",
+    "images": [
+      "https://trackmania.exchange/mapimage/177872/1"
+    ],
+    "tm": "https://trackmania.exchange/mapshow/177872",
+    "yt": "https://youtu.be/_GAwqEpkXXs",
+    "info": [
+      "Tag: SpeedTech, Scenery",
+      "Author: Ealipse",
+      "Map times: 00:48.958",
+      "Uploaded: 22 Jun 2024"
+    ]
+  },
+  {
+    "name": "Etherneal",
+    "images": [
+      "https://trackmania.exchange/mapimage/184312/1"
+    ],
+    "tm": "https://trackmania.exchange/mapshow/184312",
+    "yt": "https://youtu.be/QHqfT8qUC7U",
+    "info": [
+      "Tag: Ice, Scenery",
+      "Author: Ealipse",
+      "Map times: 00:46.387",
+      "Uploaded: 26 Jul 2024"
+    ]
+  },
+  {
+    "name": "Coincutter's Haven",
+    "images": [
+      "https://trackmania.exchange/mapimage/184806/1"
+    ],
+    "tm": "https://trackmania.exchange/mapshow/184806",
+    "yt": "https://youtu.be/KSMNbu3yEko",
+    "info": [
+      "Tag: SpeedFun, Dirt, Scenery",
+      "Author: Ealipse",
+      "Map times: 00:37.895",
+      "Uploaded: 29 Jul 2024"
+    ]
+  },
+  {
+    "name": "Slidelands 2",
+    "images": [
+      "https://trackmania.exchange/mapimage/197515/1"
+    ],
+    "tm": "https://trackmania.exchange/mapshow/197515",
+    "yt": "https://youtu.be/HIaxoWvEeW8",
+    "info": [
+      "Tag: Ice, Reactor, DesertCar",
+      "Author: Ealipse",
+      "Map times: 00:37.131",
+      "Uploaded: 23 Sept 2024"
+    ]
   }
 ];
--- script.js
+++ script.js
@@ -1,26 +1,62 @@
 const grid = document.querySelector(".grid");
 
-
 maps.forEach(map => {
 
   const videoBtn = (map.yt && map.yt.trim() !== "")
-    ? `<a href="${map.yt}" target="_blank" class="btn">Video</a>`
+    ? `<a href="${map.yt}" target="_blank" class="btn">YouTube</a>`
     : "";
 
   grid.innerHTML += `
     <div class="card" onclick="expandCard(this)">
-      <img src="${map.image}">
+      
+      <div class="card-image">
+        <img src="${map.images[0]}">
+
+        ${map.images.length > 1 ? `
+          <div class="dots">
+            ${map.images.map((_, i) => `
+              <span class="dot ${i === 0 ? "active" : ""}" 
+                    onclick="changeImage(event, this, ${i})"></span>
+            `).join("")}
+          </div>
+        ` : ""}
+      </div>
+
       <div class="card-info">
         <h3>${map.name}</h3>
       </div>
 
       <div class="card-footer">
-        <a href="${map.tm}" target="_blank" class="btn">Play</a>
-        ${videoBtn}
+
+        <div class="footer-left">
+          ${map.info.map(line => `<p>${line}</p>`).join("")}
+        </div>
+
+        <div class="footer-right">
+          <a href="${map.tm}" target="_blank" class="btn">Play</a>
+          ${videoBtn}
+        </div>
+
       </div>
+
     </div>
   `;
 });
+
+function changeImage(e, dot, index) {
+  e.stopPropagation();
+
+  const card = dot.closest(".card");
+  const img = card.querySelector("img");
+
+  const title = card.querySelector("h3").innerText;
+  const map = maps.find(m => m.name === title);
+
+  img.src = map.images[index];
+
+  card.querySelectorAll(".dot").forEach(d => d.classList.remove("active"));
+  dot.classList.add("active");
+}
 
 function expandCard(card) {
   const isActive = card.classList.contains("active");
@@ -40,3 +76,39 @@ document.addEventListener("click", (e) => {
     document.body.classList.remove("modal-active");
   }
 });
+
+
+// Discord copy name
+
+function copyDiscord(el) {
+  const text = el.childNodes[0].nodeValue.trim();
+
+  navigator.clipboard.writeText(text);
+
+  el.classList.add("copied");
+
+  const original = text;
+  el.childNodes[0].nodeValue = "Copied!";
+
+  setTimeout(() => {
+    el.childNodes[0].nodeValue = original;
+    el.classList.remove("copied");
+  }, 1200);
+}
+
+// TMX User page link
+function copyLink(e, el) {
+  // kopierer linken
+  navigator.clipboard.writeText(el.href);
+
+  // vis feedback
+  const original = el.childNodes[0].nodeValue;
+  el.childNodes[0].nodeValue = "Copied link!";
+
+  el.classList.add("copied");
+
+  setTimeout(() => {
+    el.childNodes[0].nodeValue = original;
+    el.classList.remove("copied");
+  }, 1200);
+}
